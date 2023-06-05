fad3f7a03bee41f1d9d1089c5f68a7f4420dedc3
Gani Ozturk <ganioztrk@gmail.com>
2023-06-05 03:26:11 +0300

    API sosyal medya veri cekme

---
+++ InstagramFollowerrRapidApi/InstagramFollowerrRapidApi/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http.Headers;
+using InstagramFollowerrRapidApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace InstagramFollowerrRapidApi.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public async Task<IActionResult> Index()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://instagram-profile1.p.rapidapi.com/getprofileinfo/ganioztrk"),
+                Headers =
+                {
+                    { "X-RapidAPI-Key", "57a05ced9fmsh7440ac7d8e5603fp1f46d8jsnd9e3022ec3f3" },
+                    { "X-RapidAPI-Host", "instagram-profile1.p.rapidapi.com" },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+
+                InstagramApiViewModel model = JsonConvert.DeserializeObject<InstagramApiViewModel>(body);
+                return View(model);
+            }
+        }
+
+    }
+}
---
+++ InstagramFollowerrRapidApi/InstagramFollowerrRapidApi/Models/InstagramApiViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace InstagramFollowerrRapidApi.Models
+{
+    public class InstagramApiViewModel
+    {
+        public int followers { get; set; }
+        public int following { get; set; }
+    }
+}
